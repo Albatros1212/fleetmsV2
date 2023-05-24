e888952a84efc06addf2ce5be08abc182477fa73
aziseit.pro@gmail.com <aziseit.pro@gmail.com>
2023-05-23 20:41:06 -0400

    First Commit

---
+++ fleetmsv2/src/main/java/com/azise/AppController.java
@@ -0,0 +1,14 @@
+package com.azise;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+	
+	@GetMapping("/index")
+	public String home() {
+		return "index";
+	}
+
+}
